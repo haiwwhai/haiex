118de78a61cc64aa96256ac973a56fff912e04e9
haiwwhai <30737807+haiwwhai@users.noreply.github.com>
2020-12-22 19:42:47 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python 20201222-No6-PWA.py > /dev/null 2>&1 &
+python 20201222-No6-BIT.py > /dev/null 2>&1 &
+python 20201222-No6-BIN.py > /dev/null 2>&1 &
+python 20201222-No6-GAT.py > /dev/null 2>&1 &
